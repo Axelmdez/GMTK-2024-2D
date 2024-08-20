3d2841d220ca55cb876ec4f68bd049a0db1ef927
Axel-Void <axel.mdez1@gmail.com>
2024-08-20 11:56:10 -0400

    adding in bouncy sprite particle effect and rotating platform

--- Assets/Scripts/Obstacles/RotatingPlatform.cs
+++ Assets/Scripts/Obstacles/RotatingPlatform.cs
@@ -1,23 +1,33 @@
 //scraped for simplicity
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RotatingPlatform : ObstacleBehaviour
 {
     public float rotationSpeed = 20f;
 
-    public override void DisableObstacle()
-    {
-        
-    }
-
     public override void EnableObstacle()
     {
-        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        isTriggered = false;   
     }
 
+    public void Update()
+    {
+        if (isTriggered) RotatePlatform();
+    }
 
-    
+    private void RotatePlatform()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+    }
+
+    public override void DisableObstacle()
+    {
+        isTriggered = true;    
+    } 
 }
